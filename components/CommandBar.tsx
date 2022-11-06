30a3ed6b5cb60182b600aeda297aab52cf9f9fe9
Chester How <chesterhow@gmail.com>
2022-11-06 21:17:54 +0800

    feat: use query params

---
+++ components/CommandBar.tsx
@@ -0,0 +1,49 @@
+import { Command } from "cmdk";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+
+type CommandBarProps = {
+  plants: Plant[];
+};
+
+export function CommandBar({ plants }: CommandBarProps) {
+  const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => inputRef.current?.focus(), []);
+
+  function handleSelect(value: string) {
+    router.push({
+      query: {
+        plant: encodeURI(value),
+      },
+    });
+  }
+
+  return (
+    <Command
+      label="Hello"
+      className="flex flex-col items-stretch bg-white rounded-xl overflow-hidden shadow-2xl border-[1px] border-neutral-100"
+    >
+      <Command.Input
+        ref={inputRef}
+        className="p-4 text-2xl font-light focus:outline-none border-b-[1px] border-neutral-100"
+        placeholder="Search for toxic plants..."
+        autoFocus
+      />
+      <Command.List className="p-2 overflow-y-auto h-[50vh]">
+        {plants.map((plant) => (
+          <Command.Item
+            key={plant.name}
+            value={[plant.name, plant.scientificName].join(" ")}
+            className="group px-4 py-2 aria-selected:bg-gray-100 rounded-md"
+            onSelect={handleSelect}
+          >
+            <span>{plant.name}</span>&nbsp;
+            <span className="pl-2 text-gray-400">{plant.scientificName}</span>
+          </Command.Item>
+        ))}
+      </Command.List>
+    </Command>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import { Command } from "cmdk";
 import keyBy from "lodash/keyBy";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useMemo, useRef, useState } from "react";
 
+import { CommandBar } from "../components/CommandBar";
 import { DetailLabel } from "../components/DetailLabel";
 import { DetailTagList } from "../components/DetailTagList";
 import { DetailTagListItem } from "../components/DetailTagListItem";
@@ -17,99 +19,98 @@ type HomePageProps = {
 };
 
 export default function Home({ plantsMap }: HomePageProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+  const detailRef = useRef<HTMLElement | null>(null);
+
   const plants = useMemo(() => Object.values(plantsMap), [plantsMap]);
-  const [value, setValue] = useState(Object.keys(plantsMap)[0]);
-  const [activePlant, setActivePlant] = useState<Plant>(plantsMap[value]);
+  const [activePlant, setActivePlant] = useState<Plant | null>(null);
 
+  // Update activePlant on query params change.
   useEffect(() => {
-    inputRef.current?.focus();
-  }, []);
+    // Guard: No query params.
+    if (typeof router.query.plant !== "string") {
+      setActivePlant(null);
+      return;
+    }
 
+    const plantKey = decodeURI(router.query.plant);
+    const newActivePlant = plantsMap[plantKey];
+
+    // Guards: Invalid plant in query params.
+    if (newActivePlant === undefined) {
+      setActivePlant(null);
+      return;
+    }
+
+    setActivePlant(newActivePlant);
+  }, [plantsMap, router.query]);
+
+  // Scroll detail into view on activePlant change.
   useEffect(() => {
-    setActivePlant(plantsMap[value]);
-  }, [plantsMap, value]);
+    if (activePlant !== null) {
+      detailRef.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  }, [activePlant]);
 
   return (
     <div className="flex flex-col gap-y-4 p-4 w-full max-w-screen-xl m-auto">
       <div className="bg-neutral-50 rounded-2xl p-10">
         <div className="flex flex-col gap-y-4 max-w-screen-md m-auto">
-          <h1 className="text-center">🪴 Is It Toxic To?</h1>
-
-          <Command
-            label="Hello"
-            className="flex flex-col items-stretch bg-white rounded-xl overflow-hidden shadow-2xl border-[1px] border-neutral-100"
-            value={value}
-            onValueChange={setValue}
-          >
-            <Command.Input
-              ref={inputRef}
-              className="p-4 text-2xl font-light focus:outline-none border-b-[1px] border-neutral-100"
-              placeholder="Search for toxic plants..."
-              autoFocus
-            />
-            <Command.List className="p-2 overflow-y-auto h-[50vh]">
-              {plants.map((plant) => (
-                <Command.Item
-                  key={plant.name}
-                  value={[plant.name, plant.scientificName].join(" ")}
-                  className="group px-4 py-2 aria-selected:bg-gray-100 rounded-md"
-                  onSelect={(value) => console.log(plant, value)}
-                >
-                  <span>{plant.name}</span>&nbsp;
-                  <span className="pl-2 text-gray-400">
-                    {plant.scientificName}
-                  </span>
-                </Command.Item>
-              ))}
-            </Command.List>
-          </Command>
+          <Link href="/">
+            <h1 className="text-center cursor-pointer">🪴 Is It Toxic To?</h1>
+          </Link>
+          <CommandBar plants={plants} />
         </div>
       </div>
 
-      <section className="bg-neutral-50 rounded-2xl p-10 h-[calc(100vh-2rem)]">
-        <h2>{activePlant.name}</h2>
-        <h3 className="text-2xl italic pb-10 text-neutral-500 font-light">
-          {activePlant.scientificName}
-        </h3>
+      {activePlant !== null && (
+        <section ref={detailRef} className="bg-neutral-50 rounded-2xl p-10">
+          <h2>{activePlant.name}</h2>
+          <h3 className="text-2xl italic pb-10 text-neutral-500 font-light">
+            {activePlant.scientificName}
+          </h3>
 
-        <div className="grid grid-cols-[10rem,1fr] gap-3">
-          {activePlant.family !== null && (
-            <DetailLabel title="Family" text={activePlant.family} />
-          )}
-          {activePlant.commonNames.length > 0 && (
-            <DetailTagList title="Common Names">
-              {activePlant.commonNames.map((commonName, index) => (
-                <DetailTagListItem key={index} text={commonName} />
+          <div className="grid grid-cols-[10rem,1fr] gap-3">
+            {activePlant.family !== null && (
+              <DetailLabel title="Family" text={activePlant.family} />
+            )}
+            {activePlant.commonNames.length > 0 && (
+              <DetailTagList title="Common Names">
+                {activePlant.commonNames.map((commonName, index) => (
+                  <DetailTagListItem key={index} text={commonName} />
+                ))}
+              </DetailTagList>
+            )}
+            <DetailTagList title="Toxic to">
+              {activePlant.toxicTo.map((animal, index) => (
+                <DetailTagListItem
+                  key={index}
+                  text={`${
+                    ANIMAL_EMOJI_MAP[animal]
+                  } ${animal[0].toUpperCase()}${animal
+                    .substring(1)
+                    .toLowerCase()}s`}
+                />
               ))}
             </DetailTagList>
-          )}
-          <DetailTagList title="Toxic to">
-            {activePlant.toxicTo.map((animal, index) => (
-              <DetailTagListItem
-                key={index}
-                text={`${
-                  ANIMAL_EMOJI_MAP[animal]
-                } ${animal[0].toUpperCase()}${animal
-                  .substring(1)
-                  .toLowerCase()}s`}
+            {activePlant.toxicPrinciples !== null && (
+              <DetailLabel
+                title="Toxic Principles"
+                text={activePlant.toxicPrinciples}
               />
-            ))}
-          </DetailTagList>
-          {activePlant.toxicPrinciples !== null && (
-            <DetailLabel
-              title="Toxic Principles"
-              text={activePlant.toxicPrinciples}
-            />
-          )}
-          {activePlant.clinicalSigns !== null && (
-            <DetailLabel
-              title="Clinical Signs"
-              text={activePlant.clinicalSigns}
-            />
-          )}
-        </div>
-      </section>
+            )}
+            {activePlant.clinicalSigns !== null && (
+              <DetailLabel
+                title="Clinical Signs"
+                text={activePlant.clinicalSigns}
+              />
+            )}
+          </div>
+        </section>
+      )}
     </div>
   );
 }
